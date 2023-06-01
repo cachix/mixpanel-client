03eb9b19d74469311a68a9a0f2340a2b0dc73b9f
Sander <hey@sandydoo.me>
2023-06-01 06:45:33 +0000

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+
+        haskellProjects.default = {
+          devShell.enable = true;
+
+          settings = {
+            # Skip the tests when building because the README test requires network access.
+            mixpanel-client.check = false;
+          };
+        };
+
+        packages.default = self'.packages.mixpanel-client;
+      };
+    };
+}
